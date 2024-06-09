82acad6164dfd23db427f5a2c9dc8f0f7f124399
Koshkin Nikolai <c0sx@users.noreply.github.com>
2024-06-09 13:07:47 +0400

    feat: add input controller (#1)

--- player/player.gd
+++ player/player.gd
@@ -6,63 +6,61 @@ extends CharacterBody3D
 @export var rotation_time = 0.2
 @export var moving_distance = 1.5
 
+signal on_moving_forward_command
+signal on_moving_backward_command
+signal on_strafe_left_command
+signal on_strafe_right_command
+signal on_rotation_left
+signal on_rotation_right
+
 var tween: Tween
 
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-		
-func _input(event):
-	if event.is_action("exit"):
-		get_tree().quit(0)
-		
-	if event is InputEventKey:
-		self._handle_input(event)
-	
-func _handle_input(event: InputEventKey):
-	if event.echo:
-		return;
-		
-	if event.is_action_pressed("rotate_left"):
-		_rotate_left()
-	elif event.is_action_pressed("rotate_right"):
-		_rotate_right()
-		
-	if event.is_action_pressed("forward"):
-		_move_forward()
-	elif event.is_action_pressed("backward"):
-		_move_backward()
-	elif event.is_action_pressed("left"):
-		_strafe_left()
-	elif event.is_action_pressed("right"):
-		_strafe_right()
-		
-func _move_forward(): 
-	if head.is_forward_colliding():
+func move_forward(event: InputEventKey): 
+	if not _is_input_allowed(event) or head.is_forward_colliding():
 		return
 	
 	var direction = Vector3.FORWARD
 	_move(direction)
 
-func _move_backward():
-	if head.is_backward_colliding():
+func move_backward(event: InputEventKey):
+	if not _is_input_allowed(event) or head.is_backward_colliding():
 		return
 		
 	var direction = Vector3.BACK
 	_move(direction)
 	
-func _strafe_left():
-	if head.is_left_colliding():
+func strafe_left(event: InputEventKey):
+	if not _is_input_allowed(event) or head.is_left_colliding():
 		return
 		
 	var direction = Vector3.LEFT
 	_move(direction)
 	
-func _strafe_right():
-	if head.is_right_colliding():
+func strafe_right(event: InputEventKey):
+	if not _is_input_allowed(event) or head.is_right_colliding():
 		return
 		
 	var direction = Vector3.RIGHT
 	_move(direction)
+	
+func rotate_left(event: InputEventKey):
+	if not _is_input_allowed(event):
+		return
+		
+	var direction = deg_to_rad(90)
+	
+	_rotate(direction)
+
+func rotate_right(event: InputEventKey):
+	if not _is_input_allowed(event):
+		return
+		
+	var direction = deg_to_rad(-90)
+	
+	_rotate(direction)
+	
+func _is_input_allowed(event: InputEventKey):
+	return not event.echo
 
 func _move(direction: Vector3):
 	if tween is Tween:
@@ -75,16 +73,6 @@ func _move(direction: Vector3):
 	var translated = transform.translated_local(offset)
 	
 	tween.tween_property(self, "transform", translated, self.moving_time)
-
-func _rotate_left():
-	var direction = deg_to_rad(90)
-	
-	_rotate(direction)
-
-func _rotate_right():
-	var direction = deg_to_rad(-90)
-	
-	_rotate(direction)
 	
 func _rotate(direction: float):
 	if tween is Tween:
@@ -96,4 +84,3 @@ func _rotate(direction: float):
 	var rotated = transform.basis.rotated(Vector3.UP, direction)
 	
 	tween.tween_property(self, "transform:basis", rotated, self.rotation_time)
-
